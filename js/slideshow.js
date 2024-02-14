906f53effdf62c47f47bd818d1dea65d60661592
deschdev <94028614+deschdev@users.noreply.github.com>
2024-02-13 22:53:59 -0500

    modal and mobile styling
    added the image modal and functionality.
    started mobile responsiveness.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,3 +1,4 @@
+const galleryContent = document.querySelector(".gallery-container")
 const slideshowContainer = document.querySelector(".slideshow");
 const openSlideshowButton = document.querySelector("#start-slideshow");
 const closeSlideshowButton = document.querySelector("#stop-slideshow");
@@ -6,22 +7,24 @@ const slideshowHeader = document.querySelector("#slideshow-header");
 export const slideshowFunctionality = () => {
   openSlideshowButton.addEventListener("click", openSlideshow);
   closeSlideshowButton.addEventListener("click", closeSlideshow);
-  fetchData();
+  artFetch();
 }
 
 const openSlideshow = () => {
   if (slideshowContainer) {
     slideshowContainer.classList.remove("hide");
+    galleryContent.classList.add("hide");
   }
 }
 
 const closeSlideshow = () => {
   if (slideshowContainer) {
     slideshowContainer.classList.add("hide");
+    galleryContent.classList.remove("hide");
   }
 }
 
-const fetchData = async () => {
+const artFetch = async () => {
   try {
     const response = await fetch("./data.json");
     const json = await response.json();
@@ -36,8 +39,8 @@ const fetchData = async () => {
       div.className = "slideshow-triple-split";
       div.innerHTML = `
         <div class="left">
-          <img src="${art.images.hero.small}" alt="${art.name}">
-          <button class="open-lightbox">VIEW IMAGE</button>
+          <img src="${art.images.hero.small}" alt="art by: ${art.name}">
+          <button id="open-lightbox" class="open-lightbox"> <svg width="12" height="12" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M21.7 10.3L16 4.7 4.7 16l5.7 5.7 112 112 0 0-63 63L48 208l11.3 11.3L64 224H208h16V208 64l-4.7-4.7L208 48 196.7 59.3l-63 63 0 0-112-112zm480 11.3l5.7-5.7L496 4.7l-5.7 5.7-112 112 0 0-63-63L304 48 292.7 59.3 288 64V208v16h16H448l4.7-4.7L464 208l-11.3-11.3-63-63 0 0 112-112zm-368 368l63 63L208 464l11.3-11.3L224 448V304 288H208 64l-4.7 4.7L48 304l11.3 11.3 63 63-112 112L4.7 496 16 507.3l5.7-5.7 112-112zm244.7 0l112 112 5.7 5.7L507.3 496l-5.7-5.7-112-112 63-63L464 304l-11.3-11.3L448 288H304 288v16V448l4.7 4.7L304 464l11.3-11.3 63-63zM208 70.6V208H70.6L208 70.6zm0 370.7L70.6 304H208V441.4zM441.4 304L304 441.4V304H441.4zM304 70.6L441.4 208H304V70.6z"/></svg>VIEW IMAGE</button>
         </div>
         <div class="middle">
           <div class="artist-title">
@@ -95,6 +98,46 @@ const fetchData = async () => {
         }
         createSlide(slideCounter);
       });
+
+      // Lightbox
+      const section = document.createElement("section");
+      section.id = "LIGHTBOX";
+      section.className = "image-modal hide";
+      section.innerHTML = `
+        <div class="modal-overlay"></div>
+        <div class="modal-image-container">
+          <button id="close-lightbox">CLOSE</button>
+          <img id="lightbox-image" src="" alt="">
+        </div>
+      `;
+      document.body.appendChild(section);
+
+      const LIGHTBOX = document.querySelector("#LIGHTBOX");
+      const openLightbox = document.querySelector("#open-lightbox");
+      const closeLightbox = document.querySelector("#close-lightbox");
+      const overlay = document.querySelector(".modal-overlay");
+      const lightboxImage = document.querySelector("#lightbox-image");
+
+      if (openLightbox) {
+        openLightbox.addEventListener("click", () => {
+          lightboxImage.src = art.images.gallery;
+          lightboxImage.alt = `art by: ${art.artist.name}`;
+          LIGHTBOX.classList.remove("hide");
+        });
+      }
+
+      if (closeLightbox) {
+        closeLightbox.addEventListener("click", () => {
+          LIGHTBOX.classList.add("hide");
+        });
+      }
+
+      if (overlay) {
+        overlay.addEventListener("click", () => {
+          LIGHTBOX.classList.add("hide");
+        });
+      }
+
     };
 
     createSlide(slideCounter);
